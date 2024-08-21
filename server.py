567d58e0d8998583372d8db98ca9e079ea8ead86
MLSplatoon <marcosystemssoftware@gmail.com>
2024-08-21 16:41:47 +0000

    Mundo guardado 2024-08-21

--- server.py
+++ server.py
@@ -10,7 +10,7 @@
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
-# eu		      Europa (Frankfurt)
+# eu2	      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
